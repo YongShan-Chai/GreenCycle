f330d4519790f7a72dc2c904f7d6d788979211f8
YongShan-Chai <B240193C@sc.edu.my>
2026-08-14 17:01:06 +0800

    Update AppException.java

--- src/data/AppException.java
+++ src/data/AppException.java
@@ -1,6 +1,6 @@
 package data;
 public class AppException extends Exception {
-    \\Constructor passes the error message to the parent Exception class.
+    //Constructor passes the error message to the parent Exception class.
     public AppException(String message) {
         super(message);
     }
